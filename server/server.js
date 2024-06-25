b077e24dcb7cff4057a95db8ffa065eb2ca08aaf
tudhak <tudal.haccandy@gmail.com>
2024-06-25 18:49:20 +0200

    Configured connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-// Testing
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+dotenv.config({ path: "./.env" });
+
+// Middlewares
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+let dbString = process.env.DB.replace("<PASSWORD>", process.env.DB_PASSWORD);
+dbString = dbString.replace("<USERNAME>", process.env.DB_USERNAME);
+
+mongoose
+  .connect(dbString)
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () =>
+      console.log(`Server running on Port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(err.message));
