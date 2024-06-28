aebe9255d29ef8663d653158dd448088de91f633
tudhak <tudal.haccandy@gmail.com>
2024-06-28 19:05:00 +0200

    Started to display container preview

--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -1,7 +1,18 @@
-export default function Preview() {
+export default function Preview({ container, scoops, flavors, topping }) {
   return (
-    <div className="bg-white rounded-md shadow-md w-4/5 max-w-2xl p-5">
-      <h2 className="text-3xl pacifico-regular text-center mb-10">Preview</h2>
+    <div className="bg-white rounded-md shadow-md w-4/5 max-w-2xl p-5 pb-10">
+      <h2 className="text-3xl pacifico-regular text-center mb-48">Preview</h2>
+      <div className="flex justify-center items-center">
+        {container === "Tub" && (
+          <div className="w-28 h-20 bg-gray-200 border border-black"></div>
+        )}
+        {container === "Cone" && (
+          <div
+            className="w-0 h-0 border-l-[50px] border-l-transparent border-t-[100px] border-t-orange-200
+            border-r-[50px] border-r-transparent"
+          ></div>
+        )}
+      </div>
     </div>
   );
 }
